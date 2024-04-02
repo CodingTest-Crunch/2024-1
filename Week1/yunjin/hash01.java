4b2cfed87834bd7c5625149d9f605a8c729c37fd
leeyunjin <yuncom21@ewhain.net>
2024-04-02 15:24:23 +0900

    0401

--- Week1/yunjin/hash01.java
+++ Week1/yunjin/hash01.java
@@ -5,6 +5,7 @@ import java.util.*;
 class hash01 {
     static int nums[];
 
+    // HashSet 이란 중복요소를 허용하지 않음.
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -12,33 +13,29 @@ class hash01 {
         for (int i = 0; i < nums.length; i++) {
             nums[i] = sc.nextInt();
         }
-        int ans = solution(nums);
+        int ans = solution1(nums);
         System.out.println(ans);
     }
 
-    public static int solution(int[] nums) {
+    public static int solution1(int[] nums) {
         HashSet<Integer> hs = new HashSet<>();
         for (int num : nums) {
             hs.add(num);
         }
         return Math.min(hs.size(), nums.length / 2);
     }
-}
 
-/*
- * public int solution(int[] nums) {
- * int max = nums.length / 2;
- * 
- * HashSet<Integer> hs = new HashSet<>();
- * 
- * for (int num : nums) {
- * hs.add(num);
- * }
- * int cmp = hs.size();
- * if (max >= cmp) {
- * return cmp;
- * } else {
- * return max;
- * }
- * }
- */
+    public int solution(int[] nums) {
+        int max = nums.length / 2;
+        HashSet<Integer> hs = new HashSet<>();
+        for (int num : nums) {
+            hs.add(num);
+        }
+        int cmp = hs.size();
+        if (max >= cmp) {
+            return cmp;
+        } else {
+            return max;
+        }
+    }
+}
