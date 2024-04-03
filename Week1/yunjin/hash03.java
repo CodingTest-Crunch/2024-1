0eeeaea682cad185c4e7d3b4cbb0d51699023c3c
leeyunjin <yuncom21@ewhain.net>
2024-04-03 13:32:58 +0900

    0403

--- Week1/yunjin/hash03.java
+++ Week1/yunjin/hash03.java
@@ -3,14 +3,14 @@ package Week1.yunjin;
 import java.util.*;
 
 class hash03 {
-    // 123/ 12/ 5 가 hm에 저장되었을때
-    // 1 12/ 1/ 이 hm에 존재하는지 확인하기
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         HashMap<String, Integer> hm = new HashMap<>();
         for (int i = 0; i < phone_book.length; i++) {
             hm.put(phone_book[i], 0);
         }
+        // 123/ 12/ 5 가 hm에 저장되었을때
+        // 1 12/ 1/ 이 hm에 존재하는지 확인하기
         for (int i = 0; i < phone_book.length; i++) {
             for (int j = 1; j < phone_book[i].length(); j++) {
                 if (hm.containsKey(phone_book[i].substring(0, j))) {
